21a0a2e4b1d7ab86d0b9333935a905153cd6e8c0
Jeff See <jeffsee.55@gmail.com>
2023-08-03 11:19:53 -0700

    Set up for netlify

--- functions/graphql.js
+++ functions/graphql.js
@@ -1,7 +1,13 @@
 import { databaseClient } from "../tina/__generated__/databaseClient";
 
-export default async function handler(req, res) {
-  const { query, variables } = req.body;
-  const result = await databaseClient.request({ query, variables });
-  return res.json(result);
+export async function onRequest(context) {
+  console.log("hi", context);
+  // const { query, variables } = req.body;
+  const result = await databaseClient.request({
+    query: `query {collections {__typename}}`,
+    variables: {},
+  });
+  console.log(result);
+  // return res.json(result);
+  return new Response("Hello, world!");
 }
