7f1a13a93b4139fd5e9952e5f09f94487fde938a
Jeff See <jeffsee.55@gmail.com>
2023-08-02 17:19:22 -0700

    Add database

---
+++ functions/graphql.js
@@ -0,0 +1,7 @@
+import { databaseClient } from "../tina/__generated__/databaseClient";
+
+export default async function handler(req, res) {
+  const { query, variables } = req.body;
+  const result = await databaseClient.request({ query, variables });
+  return res.json(result);
+}
--- tina/config.ts
+++ tina/config.ts
@@ -7,7 +7,7 @@ export default defineConfig({
   branch,
   clientId: null, // Get this from tina.io
   token: null, // Get this from tina.io
-
+  contentApiUrlOverride: "/graphql",
   build: {
     outputFolder: "admin",
     publicFolder: "public",
